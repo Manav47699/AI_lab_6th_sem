70e2e5d52c5c42c14149b422df7aec7a8faaffe7
Manav47699 <acharyamanav7@gmail.com>
2026-06-20 22:27:58 +0545

    inital commit

---
+++ 2_jug_problems.py
@@ -0,0 +1,20 @@
+jug4 = []
+jug3 = [1, 1, 1]
+
+jug4.append(jug3.pop())
+jug4.append(jug3.pop())
+jug4.append(jug3.pop())
+
+jug3 = [1,1 , 1]
+
+jug4.append(jug3.pop())
+
+jug4.pop()
+jug4.pop()
+jug4.pop()
+jug4.pop()
+
+jug4.append(jug3.pop())
+jug4.append(jug3.pop())
+
+print (jug4.count(1))
